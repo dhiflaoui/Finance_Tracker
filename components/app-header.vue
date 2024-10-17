7d2dd33d4efffe20b59b4e037b026d1790e2a635
dhiflaoui <belgacem.dhiflaoui@gmail.com>
2024-10-17 20:22:53 +0100

    Feat: setup project and install dependencies

---
+++ components/app-header.vue
@@ -0,0 +1,11 @@
+<template>
+  <header class="flex justify-between items-center">
+    <NuxtLink to="/" class="text-xl font-bold"> My_Finance_Tracker </NuxtLink>
+    <div>
+      <UAvatar
+        src="https://avatars.githubusercontent.com/u/739984?v=4"
+        alt="Avatar"
+      />
+    </div>
+  </header>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <!-- content will be here -->
+  </div>
+</template>
