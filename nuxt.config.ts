5ae01911e02d63ec08668d0400493ed5eac96715
dhiflaouik <belgacem.dhiflaoui@gmail.com>
2025-01-27 11:09:24 +0100

    feat: update build metadata and clean up output files

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,11 +18,11 @@ export default defineNuxtConfig({
   },
   runtimeConfig: {
     public: {
-      baseUrl:
-        process.env.NODE_ENV === "production"
-          ? process.env.SITE_URL
-          : "http://localhost:3000",
-      // baseUrl: process.env.BASE_URL ?? "http://localhost:3000",
+      // baseUrl:
+      //   process.env.NODE_ENV === "production"
+      //     ? process.env.SITE_URL
+      //     : "http://localhost:3000",
+      baseUrl: process.env.SITE_URL ?? "http://localhost:3000",
     },
   },
   nitro: {
